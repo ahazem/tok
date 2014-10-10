2b5e626be6b77590b0481c2ebfd0c511a5b2fa9a
Ahmed Hazem <ahazemm@gmail.com>
2014-10-10 03:11:25 +0200

    Add a dummy application for testing

---
+++ spec/dummy/config/initializers/secret_token.rb
@@ -0,0 +1,19 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+
+# Although this is not needed for an api-only application, rails4 
+# requires secret_key_base or secret_token to be defined, otherwise an 
+# error is raised.
+# Using secret_token for rails3 compatibility. Change to secret_key_base
+# to avoid deprecation warning.
+# Can be safely removed in a rails3 api-only application.
+Dummy::Application.config.secret_token = '900a95a13188e5451502755294494ccfb4fe7f071270faa58523fdc547b5e2bc43b511d930e7dfd22dbf972d296886760f6298e1f5266cff92fe9021be08a4cc'

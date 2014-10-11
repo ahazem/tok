68ef87588547933471ec0cf215b4f92cd2daddeb
Ahmed Hazem <ahazemm@gmail.com>
2014-10-11 23:30:37 +0200

    Add configuration option to adjust bcrypt cost

--- lib/tok/configuration.rb
+++ lib/tok/configuration.rb
@@ -1,9 +1,26 @@
+require 'bcrypt'
+
 module Tok
   class Configuration
-    attr_accessor :resource
+    attr_accessor :resource,
+                  :bcrypt_cost
 
     def resource
       @resource || ::User
+    end
+
+    def bcrypt_cost
+      if test_environment?
+        BCrypt::Engine::MIN_COST
+      else
+        @bcrypt_cost || BCrypt::Engine::DEFAULT_COST
+      end
+    end
+
+    private
+
+    def test_environment?
+      defined?(:Rails) && Rails.env.test?
     end
   end
 end

1040bb6900bc8e42a71fc8160966e9766fca2f21
Ahmed Hazem <ahazemm@gmail.com>
2014-10-14 06:02:10 +0200

    Add argument to install generator to specify model

--- lib/generators/tok/install_generator.rb
+++ lib/generators/tok/install_generator.rb
@@ -2,6 +2,7 @@ module Tok
   module Generators
     class InstallGenerator < Rails::Generators::Base
       source_root File.expand_path("../templates", __FILE__)
+      class_option :model, type: :string, aliases: "-m", desc: "Specify a model different than User"
 
       def copy_initializer
         template "tok.rb", "config/initializers/tok.rb"

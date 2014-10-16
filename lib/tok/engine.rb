001101999afa4949c73aac3a58ac6c47df53563f
Ahmed Hazem <ahazemm@gmail.com>
2014-10-16 09:07:40 +0200

    Check if user model exist (don't add engine migrations to host app load path)

--- lib/tok/engine.rb
+++ lib/tok/engine.rb
@@ -6,9 +6,17 @@ module Tok
     isolate_namespace Tok
 
     initializer :append_migrations do |app|
-      config.paths["db/migrate"].expanded.each do |expanded_path| 
-        app.config.paths["db/migrate"] << expanded_path
+      if user_model_exist?
+        config.paths["db/migrate"].expanded.each do |expanded_path| 
+          app.config.paths["db/migrate"] << expanded_path
+        end
       end
+    end
+
+    private
+
+    def user_model_exist?
+      File.exist?(File.expand_path('app/models/user.rb', Rails.root))
     end
   end
 end

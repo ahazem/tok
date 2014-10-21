15ef74d6d9fa0677bd239761099bfffebb31ddc4
Ahmed Hazem <ahazemm@gmail.com>
2014-10-21 06:03:54 +0200

    Write some specs for .bcrypt_cost configuration

--- spec/tok/configuration_spec.rb
+++ spec/tok/configuration_spec.rb
@@ -19,4 +19,39 @@ describe Tok::Configuration do
 
     it { expect(Tok.configuration.resource).to eq Account }
   end
+
+  describe "#bcrypt_cost" do
+    subject { Tok::Configuration.new.bcrypt_cost }
+
+    context "while in test environment" do
+      before do
+        Tok.configure {}
+      end
+
+      it { expect(subject).to eq BCrypt::Engine::MIN_COST }
+    end
+
+    context "while in other environments" do
+      before do
+        # Stub Rails.env
+        allow(Rails).to receive(:env).and_return(ActiveSupport::StringInquirer.new('development'))
+
+        Tok.configure {} 
+      end
+
+      it { expect(subject).to eq BCrypt::Engine::DEFAULT_COST }
+    end
+  end
+
+  describe "#bcrypt_cost=" do
+    before do
+      allow(Rails).to receive(:env).and_return(ActiveSupport::StringInquirer.new('development'))
+
+      Tok.configure do |config|
+        config.bcrypt_cost = 20
+      end
+    end
+
+    it { expect(Tok.configuration.bcrypt_cost).to eq 20 }
+  end
 end

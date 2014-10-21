c5f7c10a98e0dd80a63cabca3a23437b3a2b4500
Ahmed Hazem <ahazemm@gmail.com>
2014-10-21 08:06:10 +0200

    Create a route for /login

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,4 @@
 Rails.application.routes.draw do
   post "signup", to: "tok/users#create"
+  post "login", to: "tok/sessions#create"
 end
--- spec/routing/routes_spec.rb
+++ spec/routing/routes_spec.rb
@@ -1,6 +1,13 @@
 describe 'Tok routes' do
   context "with default routes used" do
-    it { expect(post: 'signup').to be_routable }
-    it { expect(post: 'signup').to route_to(controller: "tok/users", action: "create") }
+    describe "/signup" do
+      it { expect(post: 'signup').to be_routable }
+      it { expect(post: 'signup').to route_to(controller: "tok/users", action: "create") }
+    end
+
+    describe "/login" do
+      it { expect(post: 'login').to be_routable }
+      it { expect(post: 'login').to route_to(controller: "tok/sessions", action: "create") }
+    end
   end
 end

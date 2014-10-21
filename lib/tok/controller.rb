ae1ae0431152a0a81efd894bc5cf448cbd980f9d
Ahmed Hazem <ahazemm@gmail.com>
2014-10-21 23:57:10 +0200

    Add an authenticate! before-filter

--- lib/tok/controller.rb
+++ lib/tok/controller.rb
@@ -2,11 +2,24 @@ module Tok
   module Controller 
     extend ActiveSupport::Concern
 
-    helpers = %w(resource resource_name resource_class)
+    helpers = %w(authenticate! resource resource_name resource_class)
 
     included do
       helper_method(*helpers) 
       hide_action(*helpers)
+    end
+
+    def authenticate!
+      head :unauthorized unless authorized?
+    end
+
+    def authorized?
+      resource = resource_class.where(authentication_token: token).first
+      resource && secure_compare(resource.authentication_token, params[:token])
+    end
+
+    def token
+      request.headers["HTTP_AUTHORIZATION"] || params[:token]
     end
 
     def resource

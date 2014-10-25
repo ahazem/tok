f952d70c8f2646e7ab229fb7416e6d87ff332bc4
Ahmed Hazem <ahazemm@gmail.com>
2014-10-25 14:55:12 +0200

    Add a few more specs for /signup

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -15,6 +15,14 @@ describe Tok::UsersController do
 
       it { expect(response).to be_success }
       it { expect(response).to have_http_status(:created) }
+      it { expect(response.body).to eq User.last.to_json }
+
+      it { expect(assigns(:resource)).to be_persisted }
+      it { expect(assigns(:resource)).to eq User.last }
+
+      it "should increase users count" do
+        change(User, :count).by(1)
+      end
     end
   end
 end

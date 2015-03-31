eac4fe0a2f93637832cd34ffc91b3d73bb733857
Ahmed Hazem <ahazemm@gmail.com>
2015-03-31 05:11:40 +0200

    Add documentation in config file

--- lib/generators/tok/templates/tok.rb
+++ lib/generators/tok/templates/tok.rb
@@ -1,3 +1,38 @@
 # Use the following configuration block to adjust Tok.
 Tok.configure do |config|
+  # -- Model --
+  # If you used a model different than the already included `User` model, you
+  # have to specify the name of the model over here.
+  # Default: User
+  #
+  # config.model = 
+
+
+  # -- Bcrypt Cost --
+  # The cost factor for Bcrypt, defines how many rounds a password should be
+  # hashed.
+  # Default: 10
+  #
+  # config.bcrypt_cost = 
+ 
+
+  # -- Signup Route --
+  # The route to be used for handling creation of new users.
+  # Default: `signup`
+  #
+  # config.signup_route = 
+
+
+  # -- Login Route --
+  # The route to be used for handling authentication.
+  # Default: `login`
+  #
+  # config.login_route = 
+  
+
+  # -- Logout Route --
+  # The route to be used for logging users out.
+  # Default: `logout`
+  #
+  # config.logout_route = 
 end
